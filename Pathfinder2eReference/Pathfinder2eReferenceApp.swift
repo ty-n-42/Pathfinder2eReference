3a0ee68f8d16967b5f5849eaa2780d1673f1f0ec
Ty Newton <ty-newton@users.noreply.github.com>
2022-03-15 10:18:24 +1000

    Initial Commit

---
+++ Pathfinder2eReference/Pathfinder2eReferenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pathfinder2eReferenceApp.swift
+//  Pathfinder2eReference
+//
+//  Created by bluepill on 15/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct Pathfinder2eReferenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
